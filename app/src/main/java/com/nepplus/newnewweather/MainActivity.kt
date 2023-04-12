02e4ea874d3b0bedad55b902b54a54d7e94bd8b7
hana <onelhn@naver.com>
2023-04-13 00:17:15 +0900

    Initial commit

---
+++ app/src/main/java/com/nepplus/newnewweather/MainActivity.kt
@@ -0,0 +1,16 @@
+package com.nepplus.newnewweather
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    fun start() {
+        setContentView(R.layout.activity_start)
+
+    }
+    fun main() {
+        setContentView(R.layout.activity_main)
+        //val btn_start: Button = findViewById(R.id.btn_start)
+    }
+}
